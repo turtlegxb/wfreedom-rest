a6d846cc33ec68a1f1f44ef1e4317057ffd05204
turtlegxb <turtleforu@gmail.com>
2024-09-05 20:25:04 +0800

    update

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 buffers = {}
 decompressors = {}
 
+REPOST_MAP = {
+    '1184256142310908206' : 'https://discord.com/api/webhooks/1281227178406711368/-il3fqvexrOuv9Ws3JpobG3kblIiEEIu_wB_10avlHE46IJ76y5-4TZTD-1vzFpujuCl', # penny
+    '1238564371115020298' : 'https://discord.com/api/webhooks/1281227356551385098/yhlNXNIYmSc_0RlvHH5-V2qQH9VT45OPhzZpFAUjRVOMtQpu4fKg1QVsLcf7egOkEJfq', # aiden
+    '1121583569941315634' : 'https://discord.com/api/webhooks/1281227521534333020/wv1eZm0pu-2KTylUPRr-go_omLguJqlLy9wM1hc10Gn1bu1pZUFx0P6y68xzdDbH_UUM', # jv
+    '1181808200358580225' : 'https://discord.com/api/webhooks/1281227785553186816/jcbLyG21MakQowAT1ql9XWLi0T6gH3WRqJkhbb-mxvI2q3SnfiKGqTFppUqijwY3MuoW', # mike
+    '969226339766927410' : 'https://discord.com/api/webhooks/1281228125908500530/yFRi7p6EMg2arZGittSnK3xC9H_oTvAV2ylp0IpusrdDozM4Q7_PyJjjv0w8NWNWsuV0', # UW
+}
+
 
 def deserialize_erlpackage(payload):
     if isinstance(payload, bytes):
@@ -65,6 +73,10 @@ def printer():
 def deal_with_messsage(message):
     url = 'https://discord.com/api/webhooks/1280947188096176180/lLHcxuE6mNkykxViCIbOnUYEwiSWvUs36_MIYZ-a6ViubUfnst8t3eaDP_uJDwPi_KW_'
     if message.get('t', None) != 'MESSAGE_CREATE' or message.get('d').get('channel_id') == '1280220293759238238':
+        return
+    if message.get('channel_id') in REPOST_MAP:
+        url = REPOST_MAP[url]
+    else:
         return
     app.logger.info(message)
     msg = {
